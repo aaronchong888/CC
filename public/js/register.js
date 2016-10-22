5c86979603b65af05c2eab09c007adf7dfa49655
Jacky Mo <jackycymo@gmail.com>
2016-10-23 01:45:07 +0800

    register style try

--- public/js/register.js
+++ public/js/register.js
@@ -13,10 +13,16 @@ var Register = React.createClass({
   render: function () {
     return (
       <div className="register-container">
-        <h1>{this.state.title}</h1>
-        <div className="panel panel-default">
-          <div className="panel-body">A Basic Panel</div>
-        </div>
+        <row>
+          <div className="col-md-12 col-xs-12">
+            <h1>{this.state.title}</h1>
+          </div>
+        </row>
+        <row>
+          <div className="panel panel-default">
+            <div className="panel-body">A Basic Panel</div>
+          </div>
+        </row>
       </div>
     );
   }
