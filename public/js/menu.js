51b1cc50f1cd4fc8cfb87ef112e08ec04d5591c5
Jacky Mo <jackycymo@gmail.com>
2016-10-23 07:48:33 +0800

    acs

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,33 +1,32 @@
 
 var Menu = React.createClass({
   getInitialState: function () {
-    return ({data: "data"});
+    return ({ data: "data" });
   },
   componentDidMount: function () {
   },
   render: function () {
     return (
       <div className="bkground">
-          <div className="menuBar">
-            <div className="col-xs-2 person">
-              <i className="fa fa-user" aria-hidden="true"></i>
-            </div>
-
-            <div className="col-xs-offset-8 col-xs-2 setting">
-
-            </div>
+        <div className="col menuBar">
+          <div className="col-xs-2">
+            <i className="fa fa-user" aria-hidden="true"></i>
           </div>
+          <div className="col-xs-offset-8 col-xs-2">
+            <i className="fa fa-gear" aria-hidden="true"></i>
+          </div>
+        </div>
 
-          <div className="col-xs-offset-2 col-xs-6 match">
+        <div className="col-xs-offset-2 col-xs-6 match">
+
+        </div>
+        <div className="row col-xs-12">
+          <div className="col-xs-2 fd">
+          </div>
+          <div className="col-xs-offset-8 col-xs-2 mic">
 
           </div>
-          <div className="row col-xs-12">
-            <div className="col-xs-2 fd">
-            </div>
-            <div className="col-xs-offset-8 col-xs-2 mic">
-
-            </div>
-          </div>
+        </div>
 
       </div>
     );
