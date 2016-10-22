439c0ad12b9d1ed3c90676d6c142f7428e9ff2cf
aaronchong <aaronchong888@gmail.com>
2016-10-22 22:30:12 +0800

    fixed

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     // Our database filename
     // anything://user:password@host:port/database
     chat_db_filename: 'cc.db',
-    pg_server_url: 'postgres://postgres:postgres@localhost:5432/cc',
-    pg_local_url: 'postgres://team1@localhost:5432/cc'
+    pg_server_url: 'postgres://postgres:postgres@localhost:5432/ccdb',
+    pg_local_url: 'postgres://team1@localhost:5432/ccdb'
 };
